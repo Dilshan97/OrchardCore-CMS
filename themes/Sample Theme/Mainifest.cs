3fb88037b9d63daeac8c5a31adc97be24bbe727c
dilshan97 <dilshanramesh81@gmail.com>
2020-02-25 11:35:01 +0530

    initial commit

---
+++ themes/Sample Theme/Mainifest.cs
@@ -0,0 +1,9 @@
+﻿using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The Blog Theme",
+    Author = "The Orchard Team",
+    Website = "https://orchardproject.net",
+    Version = "2.0.0",
+    Description = "A theme adapted for blogs."
+)]
